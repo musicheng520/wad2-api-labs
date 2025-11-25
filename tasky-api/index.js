39bfb3b7c5e32dfbe10a3304424852cb674065f6
Sicheng Mu <musicheng520@gmail.com>
2025-11-25 21:59:32 +0000

    Mongo lab Integrated Tasky React App

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+// other imports
+import cors from 'cors';
 
 dotenv.config();
 
@@ -19,11 +21,17 @@ const app = express();
 
 const port = process.env.PORT;
 
+// Enable CORS for all requests
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
 
 app.use(errHandler);
+
+
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
