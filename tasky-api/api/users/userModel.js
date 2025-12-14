6cdf09db2d0a1ad877d3649674223800a7c60604
Sicheng Mu <musicheng520@gmail.com>
2025-12-14 16:44:10 +0000

    Added userId to tasks

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+},
   username: { type: String, unique: true, required: true},
   password: {type: String, required: true }
 });
